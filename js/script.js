c9ba08ebd75d7fa8117f5a3a649f093651901dab
Vinicius <vinyloucoporfutebol@hotmail.com>
2020-11-09 13:52:55 -0300

    responsivo concluido com js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function menubut() {
-    var ulmenu = document.getElementById("mylinks");
-    if (ulmenu.style.display === "flex") {
-      ulmenu.style.display = "none";
-    } else {
-      ulmenu.style.display = "flex";
-    }
-  }
+// function menubut() {
+//     var ulmenu = document.getElementById("mylinks");
+//     if (ulmenu.style.display === "flex") {
+//       ulmenu.style.display = "none";
+//     } else {
+//       ulmenu.style.display = "flex";
+//     }
+//   }
